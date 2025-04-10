b171eb11dd7a58e2f472a92e51c77e3b9541c158
Bruno Furtado <bsouzacc@gmail.com>
2025-04-10 06:36:00 -0300

    update /ajustes de estilo

--- src/app/components/Banner.tsx
+++ src/app/components/Banner.tsx
@@ -15,7 +15,7 @@ export const Banner = () => {
     return (
         <section className='banner relative w-full h-svh flex flex-col ' id='home'>
 
-            <Image src='/group1.webp' alt="Logo Image" className="absolute w-full h-full -z-0 " width={1500} height={500} />
+            <Image src='/group1.webp' alt="Logo Image" className="absolute w-full h-full -z-0 " width={1500} height={500} priority/>
 
             <div className="container relative m-auto flex items-center justify-between z-0 p-2">
               
@@ -26,7 +26,7 @@ export const Banner = () => {
                         Comece Sua Jornada nas Criptomoedas
                     </h1>
 
-                    <p className='py-10 lg:py-4 lg:w-2/3 m-auto'>
+                    <p className="py-10 lg:py-4 max-w-2xl w-full mx-auto text-center min-h-[100px]">
                         Aprenda os conceitos essenciais, descubra as melhores práticas de segurança e dê os primeiros passos para investir com confiança no mercado de criptomoedas.
                     </p>
 
