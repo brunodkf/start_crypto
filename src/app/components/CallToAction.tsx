3e3031e13ca213532ad380b431c127e56d887f00
Bruno Furtado <bsouzacc@gmail.com>
2025-06-11 23:25:45 -0300

    refactor /att deploy

--- src/app/components/CallToAction.tsx
+++ src/app/components/CallToAction.tsx
@@ -2,7 +2,6 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
-import { FaQuoteRight } from 'react-icons/fa';
 import { FiTarget } from "react-icons/fi";
 import { PiMedalLight } from "react-icons/pi";
 
